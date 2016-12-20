e357381b176a5a5fd01073a7b2bc946e3d61c7dd
Natalia <nkielbicka@softq.pl>
2016-12-20 11:47:29 +0100

    add form and addTodo() and deleteTodo()

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -6,10 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todos.component.css']
 })
 export class TodosComponent implements OnInit {
-
+  todos;
+  text;
   constructor() { }
 
   ngOnInit() {
+    this.todos = [
+      {
+        text: 'askk'
+      },
+      {
+        text: 'meet'
+      }
+    ];
   }
 
+  addTodo() {
+    this.todos.push({
+      text: this.text
+    });
+  }
+  
+  deleteTodo(todoText) {
+    for(var i = 0; i < this.todos.length; i++) {
+      if(this.todos[i].text == todoText) {
+        this.todos.splice(i,1);
+      }
+    }
+  }
 }
