05e2bb9d53b0cfed505f7b0da6afe483eeb0f3f7
Natalia <nkielbicka@softq.pl>
2016-12-28 18:53:27 +0100

    create 404 page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { TodosComponent } from './todos/todos.component';
 import { AboutMeComponent } from './about-me/about-me.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { TodoService } from './todo.service';
 
 @Component({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,21 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { TodosComponent } from './todos/todos.component';
 import { AboutMeComponent } from './about-me/about-me.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: 'todos', pathMatch: 'full' },
   { path: 'todos', component: TodosComponent },
   { path: 'about', component: AboutMeComponent },
+  { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     TodosComponent,
-    AboutMeComponent
+    AboutMeComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
