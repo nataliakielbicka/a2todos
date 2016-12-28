8afab1a00f89d56c027c29af4213ae9a3e8484ac
Natalia <nkielbicka@softq.pl>
2016-12-28 18:26:23 +0100

    new gh-pages version

--- styles.b2328beb0372c051d06d.bundle.js
+++ styles.b2328beb0372c051d06d.bundle.js
@@ -1,2 +1,2 @@
-webpackJsonp([1,2],{331:function(n,o){},604:function(n,o,c){n.exports=c(331)}},[604]);
+webpackJsonp([1,2],{369:function(n,o){},659:function(n,o,c){n.exports=c(369)}},[659]);
 //# sourceMappingURL=styles.b2328beb0372c051d06d.bundle.map
